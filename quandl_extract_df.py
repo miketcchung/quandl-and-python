d4497306b17999f2248ed2c5faca346cfaee3ff9
Mike Chung <ldsmike@gmail.com>
2021-01-05 15:33:10 -0800

    Added Python script and more images.

--- quandl_extract_df.py
+++ quandl_extract_df.py
@@ -5,3 +5,15 @@ Created on Fri Jan  1 22:13:46 2021
 @author: tccmi
 """
 
+import quandl
+
+quandl.ApiConfig.api_key = "YOUR API KEY HERE"
+
+gdp = quandl.get("FRED/GDP")
+gdp.reset_index(inplace=True)
+
+ur = quandl.get("FRED/UNRATE")
+ur.reset_index(inplace=True)
+
+ir = quandl.get("FRED/DFF")
+ir.reset_index(inplace=True)
